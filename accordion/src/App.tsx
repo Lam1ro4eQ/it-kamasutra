f7f36a5452dd7e1cdd538ee352e2475da89cfba3
Lam1ro4eQ <alsajgushev@gmail.com>
2022-09-01 16:04:20 +0300

    callbacks

--- accordion/src/App.tsx
+++ accordion/src/App.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import OnOff from "./components/OnOff/OnOff";
 import {UnControlledAccordion} from "./components/UnControlledAccordion/UnControlledAccordion";
 import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
 
 
 const App = () => {
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className={"App"}>
             <PageTitle title={"This is APP component"}/>
             <>Article 1</>
-            <Rating value={3}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+
             <Accordion titleValue={"This is Accordon 1"} collapsed={true}/>
             <Accordion titleValue={"This is Accordon 2"} collapsed={false}/>
+
             <>Article 2</>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
-            <OnOff />
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            <OnOff/>
             <UnControlledAccordion titleValue={"This is UnControlledAccordion "}/>
             <UnControlledRating/>
         </div>
