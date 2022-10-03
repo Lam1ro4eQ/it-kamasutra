57634a0e099d98332c1730fcf181627c00cd78ab
Lam1ro4eQ <alsajgushev@gmail.com>
2022-10-03 21:59:25 +0300

    select custom

--- accordion/src/components/Select/SelectDymich.tsx
+++ accordion/src/components/Select/SelectDymich.tsx
@@ -1,4 +1,4 @@
-import React, {useState, KeyboardEvent} from "react";
+import React, {useState, KeyboardEvent, useEffect} from "react";
 import style from './SelectDymich.module.css'
 
 
@@ -19,20 +19,56 @@ export function SelectDymich(props: SelectPropsType) {
     const [active, setActive] = useState(false)
     const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
     let hoveredItem = props.items.find(i => i.value === hoveredElementValue)
+
     const toggleItems = () => setActive(!active)
+    useEffect(() => {
+        setHoveredElementValue(props.value)
+    }, [props.value])
+
     const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+
+
         for (let i = 0; i < props.items.length; i++) {
-            if(hoveredElementValue === i) {
-                setHoveredElementValue(hoveredElementValue+1)
-            } if (hoveredElementValue === props.items.length - 1) {
-                setHoveredElementValue(0)
+            if (e.key === "ArrowDown" || "ArrowUp") {
+                const pretendentElement = e.key === "ArrowDown"
+                    ? props.items[i + 1]
+                    : props.items[i - 1]
+                if (props.items[i + 1]) {
+                    props.onChange(props.items[i].value)
+                    setHoveredElementValue(props.items[i].value)
+                }
+                // else {
+                //     props.onChange(props.items[0].value)
+                //     setHoveredElementValue(props.items[0].value)
+                // }
             }
+            // if (props.items[i].value === hoveredElementValue) {
+            //     if (props.items[i + 1]) {
+            //         props.onChange(props.items[i + 1].value)
+            //         setHoveredElementValue(props.items[i + 1].value)
+            //     } else {
+            //         props.onChange(props.items[0].value)
+            //         setHoveredElementValue(props.items[0].value)
+            //     }
+            // }
+            // if (e.key === "ArrowUp") {
+            //     if (props.items[i].value === hoveredElementValue) {
+            //         if (props.items[i - 1]) {
+            //             props.onChange(props.items[i - 1].value)
+            //             setHoveredElementValue(props.items[i - 1].value)
+            //         } else {
+            //             props.onChange(props.items.length)
+            //             setHoveredElementValue(props.items.length)
+            //         }
+            //     }
+            // }
+
         }
-        for (let i = 0; i < props.items.length; i++) {
-            if (props.items[i].value === hoveredElementValue) {
-                setHoveredElementValue(props.items[i+1].value)
-            }
-        }
+        // for (let i = 0; i < props.items.length; i++) {
+        //     if (props.items[i].value === hoveredElementValue) {
+        //         setHoveredElementValue(props.items[i+1].value)
+        //     }
+        // }
     }
     const onItemClick = (value: any) => {
         props.onChange(value)
