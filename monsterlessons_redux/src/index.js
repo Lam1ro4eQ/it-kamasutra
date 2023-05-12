d1422b14c218d64a171728c2b3f33d5a1e7ed599
Lam1ro4eQ <alsajgushev@gmail.com>
2023-05-12 10:37:51 +0300

    todolist-ls-11

--- monsterlessons_redux/src/index.js
+++ monsterlessons_redux/src/index.js
@@ -1,23 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-//
-// reportWebVitals();
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
 import {createStore} from "redux";
 
-function playlist(state=[], action) {
+
+const initialState = [
+    'Smaills like spirit',
+    'Enter Sandman'
+]
+
+function playlist(state = initialState, action) {
     if (action.type === 'ADD_TRACK') {
-        return  [
+        return [
             ...state,
             action.payload
         ]
@@ -28,11 +25,37 @@ function playlist(state=[], action) {
 
 const store = createStore(playlist);
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
+);
 
-store.subscribe(()=> {
-    console.log('subscribe', store.getState())
-})
+reportWebVitals();
 
-
-store.dispatch({ type: 'ADD_TRACK', payload: "Smaills like spirit"})
-store.dispatch({ type: 'ADD_TRACK', payload: "Enter Sandman"})
+// import {createStore} from "redux";
+//
+// function playlist(state=[], action) {
+//     if (action.type === 'ADD_TRACK') {
+//         return  [
+//             ...state,
+//             action.payload
+//         ]
+//     }
+//     console.log(action)
+//     return state
+// }
+//
+// const store = createStore(playlist);
+//
+//
+// store.subscribe(()=> {
+//     console.log('subscribe', store.getState())
+// })
+//
+//
+// store.dispatch({ type: 'ADD_TRACK', payload: "Smaills like spirit"})
+// store.dispatch({ type: 'ADD_TRACK', payload: "Enter Sandman"})
