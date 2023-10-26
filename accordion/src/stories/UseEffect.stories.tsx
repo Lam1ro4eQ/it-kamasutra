e89bc4af425c0ee8a1e94502c3a35396321ff101
Lam1ro4eQ <alsajgushev@gmail.com>
2023-10-26 19:05:38 +0300

    clear useEffect

--- accordion/src/stories/UseEffect.stories.tsx
+++ accordion/src/stories/UseEffect.stories.tsx
@@ -10,68 +10,114 @@ function generate() {
 }
 
 export const Example1 = () => {
-    const [counter,setCounter] = useState(1)
+    const [counter, setCounter] = useState(1)
 
     console.log("SimpleExample")
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("useEffect")
-    },[counter])
-
+    }, [counter])
 
 
     return <>
 
-        <button onClick={() => {setCounter(counter + 1)}}>+</button>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
         {counter}
     </>
 }
 
 
 export const ExampleSetTimeout = () => {
-    const [counter,setCounter] = useState(1)
+    const [counter, setCounter] = useState(1)
 
     console.log("SimpleExample")
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log("settimeout")
             document.title = counter.toString()
-        },1000)
-    },[])
-
+        }, 1000)
+    }, [])
 
 
     return <>
 
-        <button onClick={() => {setCounter(counter + 1)}}>+</button>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
         {counter}
     </>
 }
 
 
-export const ExampleSetInterval  = () => {
-    const [counter,setCounter] = useState(1)
+export const ExampleSetInterval = () => {
+    const [counter, setCounter] = useState(1)
 
     console.log("SimpleInterval")
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log("setinterval")
             setCounter(counter + 1)
-        },1000)
-    },[counter]) // или не ставить зависимость а - setCounter((state)=>{state + 1}) там замыкание и counter = 1
-
+        }, 1000)
+    }, [counter]) // или не ставить зависимость а - setCounter((state)=>{state + 1}) там замыкание и counter = 1
 
 
     return <>
 
-        <button onClick={() => {setCounter(counter + 1)}}>+</button>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        {counter}
+    </>
+}
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log("SimpleInterval")
+
+    useEffect(() => {
+        console.log("Effect occurred")
+    }, [counter]) // или не ставить зависимость а - setCounter((state)=>{state + 1}) там замыкание и counter = 1
+
+
+    return <>
+
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
         {counter}
     </>
 }
 
 
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('')
+
+    console.log("Component render with" + text)
+    const handler = (e:KeyboardEvent) => {
+        console.log(e.key)
+        setText(text + e.key)
+    }
+    useEffect(() => {
+        window.document.addEventListener('keypress', handler)
+        return () => {
+            window.document.removeEventListener('keypress', handler)
+        }
+    }, [text]) //отписка от window.document и заччистка useEffect
+
+
+    return <>
+        Typed Text: {text}
+    </>
+}
 
